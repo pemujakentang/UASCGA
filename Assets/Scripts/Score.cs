837ed10c6f4e3db3a9dde984890d83f1f4f42f40
Bonifasius Martin <martinwibawa@gmail.com>
2024-12-26 19:34:04 +0700

    Applied Power Ups to Medium and Hard scenes

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -17,7 +17,7 @@ public class Score : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (SwipeManager.tap && !PlayerManager.isGameStarted)
+        if (PlayerManager.isGameStarted)
         {
             score += Time.deltaTime;
         }
